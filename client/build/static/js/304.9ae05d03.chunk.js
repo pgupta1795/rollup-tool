213f752a59155688a0fcb5a99e66b6c5dc147a6c
pallavgupta01 <pallav.gupta@technia.com>
2022-07-03 00:47:46 +0530

    Adding build for vercel

---
+++ client/build/static/js/304.9ae05d03.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrollup_tool=self.webpackChunkrollup_tool||[]).push([[304],{304:function(e,n,l){l.r(n);var t=l(2791),u=l(6871),r=l(184),o=(0,t.lazy)((function(){return Promise.all([l.e(47),l.e(317),l.e(858),l.e(441),l.e(851)]).then(l.bind(l,5851))}));n.default=function(){var e=(0,u.UO)(),n=e.id,l=e.type;return(0,r.jsx)(t.Suspense,{children:(0,r.jsx)(o,{type:l,id:n},n)})}}}]);
+//# sourceMappingURL=304.9ae05d03.chunk.js.map
